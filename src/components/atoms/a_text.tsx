6a5fb0d45a0495a019706410f7e7f4f78164752c
FlazeFy2 <79736302+FlazeFy@users.noreply.github.com>
2026-02-11 22:20:27 +0700

    feat(auth) : add login form UI

---
+++ src/components/atoms/a_text.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+interface AtomText {
+  type: string
+  text: any
+  extraClass?: string
+}
+
+const AtomText: React.FC<AtomText> = ({ type, text, extraClass }) => {
+    if (type === 'title') {
+        return <h1 className={`${extraClass} mb-2`}>{text}</h1>
+    } else if (type === 'sub-title') {
+        return <h2 className={extraClass}>{text}</h2>
+    } else if (type === 'sub-title-small') {
+        return <h3 className={extraClass}>{text}</h3>
+    } else if (type === 'content-title') {
+        return <h4 className={extraClass}>{text}</h4>
+    } else if (type === 'content') {
+        return typeof text === "string" ? <p className={extraClass} dangerouslySetInnerHTML={{ __html: text }} /> : <p className={extraClass}>{text}</p>
+    } else if (type === 'no-content') {
+        return <p className={`italic text-secondary ${extraClass}`} style={{fontSize:"var(--textSM)"}} dangerouslySetInnerHTML={{ __html: text }}/>
+    } else if (type === 'sub-content') {
+        return <p className={`sub-content ${extraClass || ''}`}>{text}</p>
+    } else {
+        return <></>
+    }
+}
+
+export default AtomText
---
+++ src/components/organisms/login_form.organism.tsx
@@ -0,0 +1,72 @@
+"use client"
+import * as React from 'react'
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as Yup from "yup"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form"
+import { useRouter } from "next/navigation"
+import AtomText from '../atoms/a_text'
+
+// Validation
+const loginSchema = Yup.object({
+    email: Yup.string().required("Email is required").min(10).test(
+        "is-gmail",
+        "Email must end with @gmail.com",
+        (val: string) => val ? val.endsWith("@gmail.com") : false
+    ),
+    password: Yup.string().required("Password is required").min(6),
+})
+
+type LoginFormValues = Yup.InferType<typeof loginSchema>
+
+interface IOrganismsLoginFormProps {}
+
+const OrganismsLoginForm: React.FunctionComponent<IOrganismsLoginFormProps> = (props) => {
+    const router = useRouter()
+    const form = useForm<LoginFormValues>({ resolver: yupResolver(loginSchema), defaultValues: { email: "", password: "" }})
+
+    const onSubmit = async (values: LoginFormValues) => {}
+    
+    return (
+        <div className='container'>
+            <AtomText type='title' text='Welcome to EventKu' extraClass='mb-0'/>
+            <AtomText type='sub-title-small' text='Your next event starts here😉'/>
+            <hr className='mt-2 mb-10'/>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                    <FormField control={form.control} name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email Address</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Enter your email" {...field} />
+                                </FormControl>
+                                <FormMessage>{form.formState.errors.email?.message}</FormMessage>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField control={form.control} name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input type="password" placeholder="Enter your password" {...field} />
+                                </FormControl>
+                                <FormMessage>{form.formState.errors.password?.message}</FormMessage>
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit" className='mt-3' disabled={form.formState.isSubmitting}>
+                        {
+                            form.formState.isSubmitting ? "Let me check first..." : "Sign In"
+                        }
+                    </Button>
+                </form>
+            </Form>
+        </div>
+    )
+}
+
+export default OrganismsLoginForm;
