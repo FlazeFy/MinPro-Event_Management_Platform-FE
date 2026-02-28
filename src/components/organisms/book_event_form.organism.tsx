f2eaf232a64142fd8167e62c1f919e8d1b6644e2
FlazeFy2 <flazen.edu@gmail.com>
2026-02-28 20:32:00 +0700

    feat(discount) : add GET discount by event organizer id repo and integrate with UI

--- src/components/organisms/book_event_form.organism.tsx
+++ src/components/organisms/book_event_form.organism.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '../ui/button'
 import Swal from 'sweetalert2'
 import * as Yup from "yup"
@@ -13,6 +13,10 @@ import AtomDivider from '../atoms/divider.atom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import MoleculeAttendeeForm from '../molecules/attendee_form.molecule'
+import { DiscountItem, getDiscountByEventOrganizerIdRepo } from '@/repositories/r_discount'
+import MoleculeNoDataBox from '../molecules/no_data_box.molecule'
+import Skeleton from 'react-loading-skeleton'
+import MoleculeDiscountBox from '../molecules/discount_box.molecule'
 
 interface IOrganismBookEventFormProps {}
 
@@ -39,6 +43,33 @@ type BookEventFormValues = Yup.InferType<typeof bookEventSchema>
 
 const OrganismBookEventForm: React.FunctionComponent<IOrganismBookEventFormProps> = () => {
     const form = useForm<BookEventFormValues>({ resolver: yupResolver(bookEventSchema), defaultValues: { price:0, attendees: [] }})
+    // For fetching
+    const [items, setItems] = useState<DiscountItem[]>()
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState<string | null>(null)
+    const [open, setOpen] = useState(false)
+
+    const fetchDiscountByEventOrganizerId = async () => {
+        setLoading(true)
+        try {
+            const data = await getDiscountByEventOrganizerIdRepo('2034f44e-9c06-4f28-badc-6fd67a40d29d')
+            setItems(data)
+        } catch (err: any) {
+            if (err.response?.status === 404 && err.response?.data?.message) {
+                setItems([])
+                return []
+            }
+            
+            setError(err?.response?.data?.message || "Something went wrong")
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const handleOpenChange = (isOpen: boolean) => {
+        setOpen(isOpen)
+        if (isOpen) fetchDiscountByEventOrganizerId()
+    }
 
     const { fields, append, remove } = useFieldArray({
         control: form.control,
@@ -54,46 +85,59 @@ const OrganismBookEventForm: React.FunctionComponent<IOrganismBookEventFormProps
     }
 
     return (
-        <Dialog>
+        <Dialog onOpenChange={handleOpenChange} open={open}>
             <DialogTrigger asChild>
                 <Button className="h-11 rounded-full bg-indigo-600 text-sm font-semibold text-white hover:bg-indigo-700">
                     Book Tickets Now
                 </Button>            
             </DialogTrigger>
-            <DialogContent>
+            <DialogContent style={{minWidth:"1080px"}}>
                 <DialogHeader>
                     <DialogTitle>Book Event</DialogTitle>
                 </DialogHeader>
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-                        <div className='flex flex-wrap gap-2 justify-between items-center mt-5'>
-                            <AtomText text='Attendee' type='content-title'/>
-                            <Button onClick={() => append({ fullname: "", phone: "", birth_date: "" })}>
-                                <FontAwesomeIcon icon={faPlus}/>Add Attendee
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-6 max-h-[75vh] overflow-y-auto'>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                            <div className='flex flex-wrap gap-2 justify-between items-center mt-5'>
+                                <AtomText text='Attendee' type='content-title'/>
+                                <Button onClick={() => append({ fullname: "", phone: "", birth_date: "" })}>
+                                    <FontAwesomeIcon icon={faPlus}/>Add Attendee
+                                </Button>
+                            </div>
+                            {
+                                fields.map((field, index) => (
+                                    <MoleculeAttendeeForm key={field.id} index={index} register={form.register} errors={form.formState.errors} onDelete={() => remove(index)}/>
+                                ))
+                            }
+                            <AtomDivider/>
+                            <FormField control={form.control} name="price"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Price</FormLabel>
+                                        <FormControl>
+                                            <Input type="number" disabled={true} {...field} min={1}/>
+                                        </FormControl>
+                                        <FormMessage>{form.formState.errors.price?.message}</FormMessage>
+                                    </FormItem>
+                                )}
+                            />
+                            <Button type="submit" disabled={form.formState.isSubmitting}>
+                                { form.formState.isSubmitting ? "Sending..." : "Book this Event" }
                             </Button>
-                        </div>
+                        </form>
+                    </Form>
+                    <div>
+                        <AtomText type='content-title' text='Available Discount' extraClass='mb-2'/>
+                        { error && <MoleculeNoDataBox title='Something went wrong'/> }
+                        { loading && <Skeleton className="h-[200px] w-full rounded-xl" /> }
                         {
-                            fields.map((field, index) => (
-                                <MoleculeAttendeeForm key={field.id} index={index} register={form.register} errors={form.formState.errors} onDelete={() => remove(index)}/>
-                            ))
+                            !loading && !error && items && items.length > 0 ?
+                                items.map((dt, idx) => <MoleculeDiscountBox key={idx} description={dt.description} percentage={dt.percentage} expiredAt={dt.expired_at} role={'customer'}/>)
+                            :
+                                <MoleculeNoDataBox title={'No discount found'}/>
                         }
-                        <AtomDivider/>
-                        <FormField control={form.control} name="price"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Price</FormLabel>
-                                    <FormControl>
-                                        <Input type="number" disabled={true} {...field} min={1}/>
-                                    </FormControl>
-                                    <FormMessage>{form.formState.errors.price?.message}</FormMessage>
-                                </FormItem>
-                            )}
-                        />
-                        <Button type="submit" disabled={form.formState.isSubmitting} className='w-full'>
-                            { form.formState.isSubmitting ? "Sending..." : "Book this Event" }
-                        </Button>
-                    </form>
-                </Form>
+                    </div>
+                </div>
             </DialogContent>
         </Dialog>
     )
